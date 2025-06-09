615e9ea3cee5da190f324453488152293ffd0d84
PaulFirs <makloun@hotmail.com>
2025-06-09 22:21:37 +0300

    Init

---
+++ src/extension.ts
@@ -0,0 +1,134 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { RPLCs, RPLCConfig } from './webviews/config_presets';
+
+export function activate(context: vscode.ExtensionContext) {
+    const view = vscode.window.createTreeView('rhs-sdk.tools', {
+        treeDataProvider: new ToolsProvider(context),
+        showCollapseAll: true
+    });
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('rhs-sdk.tools.open', (pageName: string) => {
+            openHtmlPage(context, pageName);
+        })
+    );
+}
+
+const openPanels: Record<string, vscode.WebviewPanel | undefined> = {};
+
+function openHtmlPage(context: vscode.ExtensionContext, pageName: string) {
+
+    const existingPanel = openPanels[pageName];
+
+    if (existingPanel) {
+        existingPanel.reveal();
+        return;
+    }
+
+    const panel = vscode.window.createWebviewPanel(
+        `toolsPage.${pageName}`,
+        pageName,
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true,
+            retainContextWhenHidden: true
+        }
+    );
+
+    openPanels[pageName] = panel;
+
+    panel.onDidDispose(() => {
+        openPanels[pageName] = undefined;
+    });
+
+    panel.webview.onDidReceiveMessage(
+        message => {
+            switch (message.command) {
+                case 'rplcListChanged':
+                    vscode.window.showInformationMessage(`Selected value: ${message.value}`);
+                    panel.webview.postMessage({
+                        command: 'RPLC_Presets',
+                        presets: RPLCs.find(rplc => rplc.name === message.value)
+                    });
+                    break;
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+
+    const htmlPath = path.join(context.extensionPath, 'src', 'webviews', `${pageName}.html`);
+
+    fs.readFile(htmlPath, 'utf8', (err, content) => {
+        if (err) {
+            panel.webview.html = `<!DOCTYPE html>
+                <html>
+                <body>
+                    <h1>Error loading page</h1>
+                    <p>${err.message}</p>
+                </body>
+                </html>`;
+            return;
+        }
+
+        const resourcePath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'webviews'));
+
+        content = content.replace(/(src|href)="([^"]*)"/g, (match, p1, p2) => {
+            return `${p1}="${panel.webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'src', 'webviews'
+                , p2)))}"`;
+        });
+
+        panel.webview.html = content;
+    });
+}
+
+class ToolsProvider implements vscode.TreeDataProvider<ToolItem> {
+    constructor(private context: vscode.ExtensionContext) { }
+
+    getTreeItem(element: ToolItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: ToolItem): Thenable<ToolItem[]> {
+        if (element) {
+            return Promise.resolve([]);
+        } else {
+            return Promise.resolve([
+                new ToolItem('Creator', 'creator', {
+                    command: 'rhs-sdk.tools.open',
+                    title: '',
+                    arguments: ['creator']
+                }),
+                new ToolItem('Editor', 'editor', {
+                    command: 'rhs-sdk.tools.open',
+                    title: '',
+                    arguments: ['editor']
+                }),
+                new ToolItem('Logger', 'logger', {
+                    command: 'rhs-sdk.tools.open',
+                    title: '',
+                    arguments: ['logger']
+                })
+            ]);
+        }
+    }
+}
+
+class ToolItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        private pageName: string,
+        public readonly command: vscode.Command
+    ) {
+        super(label, vscode.TreeItemCollapsibleState.None);
+        this.tooltip = `${this.label}`;
+        this.description = '';
+    }
+
+    iconPath = {
+        light: vscode.Uri.file(path.join(__dirname, '..', 'media', 'light', 'tool.svg')),
+        dark: vscode.Uri.file(path.join(__dirname, '..', 'media', 'dark', 'tool.svg'))
+    };
+}
---
+++ src/webviews/config_presets.ts
@@ -0,0 +1,90 @@
+export interface RPLCConfig {
+        name: string;
+        hal: {
+                flash_ex?: boolean;
+                io?: boolean;
+                rtc?: boolean;
+                serial?: boolean;
+                speaker?: boolean;
+                can?: boolean;
+                random?: boolean;
+                usb?: boolean;
+                network?: boolean;
+        };
+        services: {
+                notification?: boolean;
+                lwip?: boolean;
+                can_open?: boolean;
+                usb_serial_bridge?: boolean;
+        };
+        tests: {
+                memmng?: boolean;
+                flash_ex?: boolean;
+        };
+        compileDefinition: string;
+}
+
+export const RPLCs: RPLCConfig[] = [
+        {
+                name: "RPLC_XL",
+                hal: {
+                        flash_ex: true,
+                        io: true,
+                        rtc: true,
+                        serial: true,
+                        speaker: true,
+                        can: true,
+                        random: true,
+                        // network: true, // Uncomment if needed
+                },
+                services: {
+                        notification: true,
+                        lwip: true,
+                        can_open: true,
+                },
+                tests: {
+                        memmng: true,
+                        flash_ex: true,
+                },
+                compileDefinition: "RPLC_XL"
+        },
+        {
+                name: "RPLC_L",
+                hal: {
+                        flash_ex: true,
+                        io: true,
+                        rtc: true,
+                        serial: true,
+                        speaker: true,
+                        can: true,
+                        random: true,
+                },
+                services: {
+                        notification: true,
+                        can_open: true,
+                },
+                tests: {
+                        memmng: true,
+                        flash_ex: true,
+                },
+                compileDefinition: "RPLC_L"
+        },
+        {
+                name: "RPLC_M",
+                hal: {
+                        serial: true,
+                        speaker: true,
+                        can: true,
+                        usb: true,
+                },
+                services: {
+                        notification: true,
+                        usb_serial_bridge: true,
+                        can_open: true,
+                },
+                tests: {
+                        memmng: true,
+                },
+                compileDefinition: "RPLC_M"
+        }
+];
